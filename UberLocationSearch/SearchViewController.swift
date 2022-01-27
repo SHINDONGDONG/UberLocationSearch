d7ff42092b80de5038c71a1656aeeb7577663192
mincheol <homerjay@naver.com>
2022-01-27 18:02:41 +0900

    SearchViewController, FloatingPanelController, set,addpanel

--- UberLocationSearch/SearchViewController.swift
+++ UberLocationSearch/SearchViewController.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    //UI Label로 라벨을 작성 한다.
+    private let label: UILabel = {
+       let label = UILabel()
+        label.text = "Where to?"
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
+        return label
+    }()
+    
+    //UITextfield를 선언.
+    private let field: UITextField = {
+        let field = UITextField()
+        //placeholder
+        field.placeholder = "Enter Description"
+        //textfield의 모서리 둥글게
+        field.layer.cornerRadius = 9
+        //알아가보자
+        field.backgroundColor = .tertiarySystemBackground
+        //uiview로 넓이는 10 높이는 50이다.
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+        return field
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //system background컬러로 설정후
+        view.backgroundColor = .secondarySystemBackground
+        //위에서 label만든것을 addsubview에 추가시켜준다.
+        view.addSubview(label)
+        view.addSubview(field)
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //subview가 표시될 때 마다 갱신함.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        label.sizeToFit()
+        //라벨의 프레임 위치.
+        label.frame = CGRect(x: 10, y: 10, width: label.frame.size.width, height: label.frame.size.height)
+        //field의 프레임은 label보다 밑에 와야하기 때문에 y가 20+ 라벨위치이다          //사이즈도 view에서 20보다 작게 높이는 50으로 고정
+        field.frame = CGRect(x: 10, y: 20+label.frame.size.height, width: view.frame.size.width-20, height: 50)
     }
-    */
-
 }
--- UberLocationSearch/ViewController.swift
+++ UberLocationSearch/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 //Apple MapKit을 사용할 수 있게 imort해준다.
 import MapKit
+import FloatingPanel
 
 class ViewController: UIViewController {
 
@@ -16,8 +17,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //view위에 addview로 mapview를 깔아준다.
+        //view위에 ad dview로 mapview를 깔아준다.
         view.addSubview(mapView)
+        //Navi바의 타이틀을 넣어줌으로 써 좀더 멋을 살렸다. 지림
+        self.title = "Uber"
+        
+        //Floatingpanel컨트롤러를 선언.
+        let panel = FloatingPanelController()
+        panel.set(contentViewController:  SearchViewController())
+        //viewcontroller가 부모이고 to로 가는곳이 Search 컨트롤러
+        panel.addPanel(toParent: self)
+        
+        
+        
     }
     
     //서브뷰가 셋업되고 난 후 호출됨.
